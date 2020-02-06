a9eb066b6603255f156eefed90fc3f303c24e0ef
andrewjohnysons <andrew.johnys@ext.ons.gov.uk>
2020-02-06 09:55:22 +0000

    Added NewAddressReported Event

---
+++ src/main/java/uk/gov/ons/ctp/common/event/model/NewAddress.java
@@ -0,0 +1,14 @@
+package uk.gov.ons.ctp.common.event.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class NewAddress {
+
+  private String sourceCaseId;
+  private CollectionCase collectionCase;
+}
