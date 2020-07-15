503bb4f210c3b1639e6d92998417d87352100ccc
Peter Bochel <Peter.Bochel@ext.ons.gov.uk>
2020-07-15 11:15:17 +0100

    CR-1063 Workaround for Lombok vs. Jackson serialsation

--- src/main/java/uk/gov/ons/ctp/common/event/model/RespondentRefusalDetails.java
+++ src/main/java/uk/gov/ons/ctp/common/event/model/RespondentRefusalDetails.java
@@ -1,9 +1,12 @@
 package uk.gov.ons.ctp.common.event.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Data
 @NoArgsConstructor
@@ -14,10 +17,27 @@ public class RespondentRefusalDetails implements EventPayload {
   private String agentId;
   private String callId;
 
-  @JsonProperty(value = "isHouseholder")
+  @Getter(AccessLevel.NONE)
+  @Setter(AccessLevel.NONE)
   private boolean isHouseholder;
 
   private CollectionCaseCompact collectionCase;
   private ContactCompact contact;
   private AddressCompact address;
+
+  // The explicit get and set methods workaround a problem caused by the interaction of Lombok and
+  // Jackson.
+  // By default lombok creates an isHouseholder() method, but then Jackson serialises this without
+  // the 'is' part.
+  // BTW, a JsonProperty annotation still leaves the isHouseholder() method visible, so the
+  // generated Json contains a 'isHouseholder' and a 'householder' field.
+  @JsonProperty(value = "isHouseholder")
+  public boolean isHouseholder() {
+    return isHouseholder;
+  }
+
+  @JsonProperty(value = "isHouseholder")
+  public void setHouseholder(boolean isHouseholder) {
+    this.isHouseholder = isHouseholder;
+  }
 }
